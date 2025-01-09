b1e47a130582fa35db2a2a4458b7013045125b18
3boodev <a.abdelsamad.al@gmail.com>
2025-01-10 01:37:14 +0200

    Initial commit

---
+++ lib/sections/about/desktopAbout.dart
@@ -0,0 +1,195 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:fluttertoast/fluttertoast.dart';
+import 'package:my_portfolio/constants.dart';
+import 'package:my_portfolio/provider/themeProvider.dart';
+import 'package:my_portfolio/widget/aboutMeMetaData.dart';
+import 'package:my_portfolio/widget/adaptiveText.dart';
+import 'package:my_portfolio/widget/communityIconBtn.dart';
+import 'package:my_portfolio/widget/customBtn.dart';
+import 'package:my_portfolio/widget/customTextHeading.dart';
+import 'package:my_portfolio/widget/toolsTechWidget.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:provider/provider.dart';
+
+import 'package:universal_html/html.dart' as html;
+
+class AboutDesktop extends StatelessWidget {
+  final _communityLogoHeight = [50.0, 50.0, 50.0];
+
+  @override
+  Widget build(BuildContext context) {
+    final _themeProvider = Provider.of<ThemeProvider>(context);
+    double height = MediaQuery.of(context).size.height;
+    double width = MediaQuery.of(context).size.width;
+
+    return Container(
+      padding: EdgeInsets.symmetric(
+        horizontal: width * 0.02,
+      ),
+      color: _themeProvider.lightTheme ? Colors.white : Colors.black,
+      child: Column(
+        children: [
+          // CustomSectionHeading(text: "\nAbout Me"),
+          // CustomSectionSubHeading(text: "Get to know me ❤️"),
+          SizedBox(height: 30.0),
+          Row(
+            children: [
+              // Expanded(
+              //   child: Image.asset(
+              //     'assets/web.png',
+              //     height: height * 0.7,
+              //   ),
+              // ),
+              Expanded(
+                flex: width < 1230 ? 2 : 1,
+                child: Container(
+                  padding: EdgeInsets.only(left: width < 1230 ? 25.0 : 0),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      AdaptiveText(
+                        "Who am I?",
+                        style: GoogleFonts.montserrat(
+                            color: kPrimaryColor, fontSize: height * 0.025),
+                      ),
+                      SizedBox(
+                        height: height * 0.03,
+                      ),
+                      AdaptiveText(
+                        "I'm Abdullah Alamary, SoftWare Engineer as Mobile Developer.",
+                        style: GoogleFonts.montserrat(
+                          fontSize: height * 0.035,
+                          fontWeight: FontWeight.w400,
+                          color: _themeProvider.lightTheme
+                              ? Colors.black
+                              : Colors.white,
+                        ),
+                      ),
+                      SizedBox(
+                        height: height * 0.02,
+                      ),
+                      AdaptiveText(
+                        "📌 My Current Work as Senior Software Engineer @ Aloo Software Company in KSA. \n📌 Have More than 6 Apps Uploaded on AppStore & Google Play Store \n📌 I'm Interest to BI & Big Data  Development",
+                        style: GoogleFonts.montserrat(
+                          fontSize: height * 0.02,
+                          color: Colors.grey[500],
+                          height: 2.0,
+                        ),
+                      ),
+                      SizedBox(
+                        height: height * 0.025,
+                      ),
+                      Container(
+                        decoration: BoxDecoration(
+                          border: Border(
+                            bottom:
+                                BorderSide(color: Colors.grey[800]!, width: 2.0),
+                          ),
+                        ),
+                      ),
+                      SizedBox(
+                        height: height * 0.02,
+                      ),
+                      AdaptiveText(
+                        "Technologies I have worked with:",
+                        style: GoogleFonts.montserrat(
+                            color: kPrimaryColor, fontSize: height * 0.018),
+                      ),
+                      Row(
+                        children: [
+                          for (int i = 0; i < kTools.length; i++)
+                            ToolTechWidget(
+                              techName: kTools[i],
+                            ),
+                        ],
+                      ),
+                      SizedBox(
+                        height: height * 0.02,
+                      ),
+                      Container(
+                        decoration: BoxDecoration(
+                          border: Border(
+                            bottom:
+                                BorderSide(color: Colors.grey[800]!, width: 2.0),
+                          ),
+                        ),
+                      ),
+                      SizedBox(
+                        height: height * 0.025,
+                      ),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          AboutMeMetaData(
+                            data: "Name",
+                            information: "Abdullah Alamary",
+                          ),
+                          Row(
+                            crossAxisAlignment: CrossAxisAlignment.center,
+                            children: [
+                              AboutMeMetaData(
+                                data: "Email",
+                                information: "a.abdelsamad.al@gmail.com",
+                                alignment: Alignment.centerLeft,
+                              ),
+                              SizedBox(width: 10,),
+                              IconButton(onPressed: (){
+                                Clipboard.setData(
+                                    ClipboardData(
+                                        text:
+                                        'a.abdelsamad.al@gmail.com'));
+                                Fluttertoast.showToast(
+                                    msg:'Copied');
+                              }, icon: Icon(Icons.copy,size: 15,))
+                            ],
+                          ),
+                        ],
+                      ),
+                      SizedBox(
+                        height: height * 0.02,
+                      ),
+                      Row(
+                        children: [
+                          Padding(
+                            padding: const EdgeInsets.all(8.0),
+                            child: OutlinedCustomBtn(
+                              onPressed: () {
+                                html.window.open(
+                                    'https://drive.google.com/file/d/1pYHFjUWNU0tZY0bBvKucAq5aZwy0H4kr/view?usp=sharing',
+                                    "pdf");
+                              },
+                              btnText: "Resume",
+                            ),
+                          ),
+                          Container(
+                            width: width * 0.05,
+                            decoration: BoxDecoration(
+                              border: Border(
+                                bottom: BorderSide(
+                                    color: Colors.grey[900]!, width: 2.0),
+                              ),
+                            ),
+                          ),
+                          for (int i = 0; i < kCommunityLogo.length; i++)
+                            CommunityIconBtn(
+                              icon: kCommunityLogo[i],
+                              link: kCommunityLinks[i],
+                              height: _communityLogoHeight[i],
+                            ),
+                        ],
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+              Container(
+                width: width < 1230 ? width * 0.05 : width * 0.1,
+              ),
+            ],
+          )
+        ],
+      ),
+    );
+  }
+}
